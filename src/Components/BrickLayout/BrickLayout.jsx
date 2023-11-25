ae561b7dfa793336ff59dccc47b9cff888ca6930
JM <jnmrles@gmail.com>
2023-11-25 05:49:58 -0600

    added images to portfolio

--- src/Components/BrickLayout/BrickLayout.jsx
+++ src/Components/BrickLayout/BrickLayout.jsx
@@ -5,13 +5,13 @@ const BrickLayout = () => {
     <div className={styles["brick-layout"]}>
       <div className={styles["brick-column"]}>
         <img src="img/dummy_image/karol.png" alt="" />
-        <img src="img/dummy_image/2.jpg" alt="" />
+        <img src="img/wallpapers/test.png" alt="" />
         <img src="img/dummy_image/3.jpg" alt="" />
       </div>
       <div className={styles["brick-column"]}>
         <img src="img/dummy_image/flame.jpg" alt="" />
-        <img src="img/dummy_image/karol.png" alt="" />
-        <img src="img/dummy_image/6.jpg" alt="" />
+        <img src="img/wallpapers/uzi3.png" alt="" />
+        <img src="img/wallpapers/keyglock.jpg" alt="" />
       </div>
       <div className={styles["brick-column"]}>
         <img src="img/dummy_image/7.jpg" alt="" />
