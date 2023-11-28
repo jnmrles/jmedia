c23ae2c630b0c59aeca01840778cc6c9a0f0e6ba
JM <jnmrles@gmail.com>
2023-11-28 00:49:42 -0600

    added zoom functionality

--- src/Components/MasonryLayout/MasonryLayout.jsx
+++ src/Components/MasonryLayout/MasonryLayout.jsx
@@ -6,7 +6,9 @@ import Masonry from "react-masonry-css";
 
 // import other component to use
 import MasonryBox from "./MasonryBox/MasonryBox";
+import Zoom from "react-medium-image-zoom";
 
+import React, { useState, useEffect } from "react";
 // MasonryLayout Component
 const MasonryLayout = ({ images }) => {
   const breakpointColumnsObj = {
@@ -16,22 +18,71 @@ const MasonryLayout = ({ images }) => {
     500: 1,
   };
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      if (modalOpen) {
+        event.preventDefault();
+      }
+    };
+
+    if (modalOpen) {
+      document.body.style.overflow = "hidden";
+      window.addEventListener("wheel", handleScroll, { passive: false });
+    } else {
+      document.body.style.overflow = "auto";
+      window.removeEventListener("wheel", handleScroll);
+    }
+
+    return () => {
+      document.body.style.overflow = "auto";
+      window.removeEventListener("wheel", handleScroll);
+    };
+  }, [modalOpen]);
+
+  const openModal = (image) => {
+    setSelectedImage(image);
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
-    <Masonry
-      breakpointCols={breakpointColumnsObj}
-      className={styles["my-masonry-grid"]}
-      columnClassName={styles["my-masonry-grid_column"]}
-    >
-      {images.map((item) => (
-        <MasonryBox
-          key={item.id}
-          wallSrc={item.src}
-          userProf={item.user.src}
-          userName={item.user.name}
-          userJob={item.user.job}
-        />
-      ))}
-    </Masonry>
+    <>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className={styles["my-masonry-grid"]}
+        columnClassName={styles["my-masonry-grid_column"]}
+      >
+        {images.map((item, index) => (
+          <div key={index} onClick={() => openModal(item.src)}>
+            <MasonryBox
+              key={item.id}
+              wallSrc={item.src}
+              userProf={item.user.src}
+              userName={item.user.name}
+              userJob={item.user.job}
+            />
+          </div>
+        ))}
+      </Masonry>
+      {modalOpen && (
+        <div className={styles["modal"]}>
+          <span className={styles["close"]} onClick={closeModal}>
+            &times;
+          </span>
+          <img
+            className={styles["modal-content"]}
+            src={selectedImage}
+            alt="Zoomed Image"
+          />
+        </div>
+      )}
+    </>
   );
 };
 
